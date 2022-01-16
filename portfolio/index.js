e2d6f311026af39b8cf1eb9986f3db18c2776e95
Anton Novikov <3483778@gmail.com>
2022-01-16 12:50:21 +0300

    feat: add index.js with self-review

---
+++ portfolio/index.js
@@ -0,0 +1,8 @@
+console.log(
+    "1. Valid layout: W3C check is OK. (10 points)\n"+
+    "2. Semantic layout - OK. (20 points)\n"+
+    "3. Layout matches design. (48 points)\n"+
+    "4. CSS requrements met: used flex, while scaling site stay centered, background color are 100% of width, svg/jpg/fav are present. (12 points)\n"+
+    "5. CSS interactions applied: smooth scolling to anchors, footer links, hover actions for elements, smooth transitions for hover actions. (20 points)\n"+
+    "TOTAL: 110 points :)"
+)
